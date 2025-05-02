b1d7a550f1bd501f09b8a4b0e38bcc99d1e5486b
samps <sampsonkeckmatthew@gmail.com>
2025-05-02 12:22:21 -0600

    updated the correct url

--- content/src/utils.js
+++ content/src/utils.js
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://o11ny20zld.execute-api.us-east-1.amazonaws.com/dev/';
-export const API_URL = 'http://localhost:5000/'
+export const API_URL = 'https://o11ny20zld.execute-api.us-east-1.amazonaws.com/dev/';
+// export const API_URL = 'http://localhost:5000/'
